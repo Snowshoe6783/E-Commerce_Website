c1f7532440ccc9a6e3a1954e76fd4bfe6d50b837
Alle-marchio <78793701+Alle-marchio@users.noreply.github.com>
2022-04-15 16:17:52 +0200

    modificato il t di refresh Login

--- E-Commerce/Pagine/Login/login.php
+++ E-Commerce/Pagine/Login/login.php
@@ -84,7 +84,7 @@
 						$_SESSION['ruolo_ID'] = $ruolo_ID;
 						$_SESSION['utente_ID'] = $utente_ID;
 						echo "Login effettuato. Ti riporto alla pagina iniziale...";
-						header( "Refresh:3; url = ../Index/index.php", true, 303);
+						header( "Refresh:1; url = ../Index/index.php", true, 303);
 					}else{
 						echo "Login fallito, username e/o password sbagliata.";
 					}
