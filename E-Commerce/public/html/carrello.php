9ab9cd4b8fbf032a67ffe2641bc7928944578c39
Marprog-jpg <78869306+Marprog-jpg@users.noreply.github.com>
2022-04-29 18:31:13 +0200

    Aggiunti la possibilità di cambiare prodotti nel carrello.

--- E-Commerce/public/html/carrello.php
+++ E-Commerce/public/html/carrello.php
@@ -70,14 +70,11 @@ $_SESSION['prezzo_prodotti_totale'] = 0;
 
 	<?php
 
-
-
-		$result->fetch_all(MYSQLI_ASSOC);
 		foreach ($result as $row) {
 
 			$quadro_ID = $row['quadro_ID'];
 			if ($flag == 0) {
-				$query = "SELECT nome_quadro AS 'Nome Quadro', nome_autore AS Autore, genere AS Genere, descrizione_breve AS Descrizione, prezzo as Prezzo, quantita AS Quantità
+				$query = "SELECT nome_quadro AS 'Nome Quadro', nome_autore AS Autore, genere AS Genere, descrizione_breve AS Descrizione, prezzo as Prezzo, quantita AS Quantità, q.quantita_in_magazzino AS quantita_in_magazzino, a.quadro_ID AS quadro_ID
 							 FROM quadro AS q JOIN acquisto AS a
 							 ON q.quadro_ID = a.quadro_ID
 							 WHERE q.quadro_ID = '$quadro_ID'
@@ -89,7 +86,7 @@ $_SESSION['prezzo_prodotti_totale'] = 0;
 				//echo "<br><br>start".$query;
 
 			} else {
-				$query = "SELECT nome_quadro AS 'Nome Quadro', nome_autore AS Autore, genere AS Genere, descrizione_breve AS Descrizione, prezzo as Prezzo, quantita AS Quantità
+				$query = "SELECT nome_quadro AS 'Nome Quadro', nome_autore AS Autore, genere AS Genere, descrizione_breve AS Descrizione, prezzo as Prezzo, quantita AS Quantità, q.quantita_in_magazzino AS quantita_in_magazzino, a.quadro_ID AS quadro_ID
 						  FROM quadro AS q JOIN acquisto AS a
 						  ON q.quadro_ID = a.quadro_ID
 						  WHERE q.quadro_ID = '$quadro_ID'
@@ -108,32 +105,46 @@ $_SESSION['prezzo_prodotti_totale'] = 0;
 
 		$prezzo_totale = 0;
 		$result = $conn->query($query);
+		$result_quadri_in_carrello = $result;
 
 		foreach ($result as $row) {
 			echo "<tr>";
 			foreach ($row as $key => $value) {
-				echo "<th>$key</th>";
+				if (($key != "quantita_in_magazzino") && ($key != "quadro_ID")) {
+					echo "<th>$key</th>";
+				}
 			}
 			echo "<th>Prezzo Totale</th>";
 			echo "</tr>";
 			break;
 		}
-
+		echo "<form method=\"post\" name=\"inizia_ordine\">";
 		foreach ($result as $row) {
 			echo "<tr>";
+			$quadro_ID = $row['quadro_ID'];
 			$nome_quadro = $row['Nome Quadro'];
 			$nome_autore = $row['Autore'];
 			$genere = $row['Genere'];
 			$descrizione_breve = $row['Descrizione'];
 			$prezzo = $row['Prezzo'];
 			$quantita = $row['Quantità'];
+			$quantita_in_magazzino = $row['quantita_in_magazzino'];
 
 			echo "<td>$nome_quadro</td>";
 			echo "<td>$nome_autore</td>";
 			echo "<td>$genere</td>";
 			echo "<td>$descrizione_breve</td>";
 			echo "<td>$prezzo</td>";
-			echo "<td>$quantita</td>";
+			echo "<td>
+				<select name = \"$quadro_ID\" id = \"quantita\">";
+
+
+			for ($i = 1; $i < $quantita_in_magazzino; $i++) {
+				if ($i == $quantita) {
+					echo "<option value = " . $quantita . " selected>" . $quantita . "</option>";
+				}
+				echo "<option value = " . $i . ">" . $i . "</option>";
+			}
 			echo "<td>" . $prezzo * $quantita . "</td>";
 
 			echo "</tr>";
@@ -149,6 +160,26 @@ $_SESSION['prezzo_prodotti_totale'] = 0;
 	}
 
 
+	?>
+		<input type = "submit" name = "submit_inizio_ordine">
+	</form>
+
+	<?php
+		if(isset($_POST['submit_inizio_ordine'])){
+			foreach($result_quadri_in_carrello as $row){
+				$quadro_ID = $row['quadro_ID'];
+				$query = "UPDATE acquisto
+					      SET quantita = '" . $_POST[$quadro_ID] . "'
+						  WHERE quadro_ID = '$quadro_ID'
+						  AND ordine_ID = '" . $_SESSION['ordine_ID'] . "'";;
+
+				$result = $conn->query($query);
+				//echo $query . "<br>";
+			}
+			//header("location:carrello.php");
+			echo("<script>location.href = 'indirizzo.php';</script>");
+
+		}
 	?>
 </body>
 
